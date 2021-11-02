09f68ce6c8cc2d2f8754a07e679d1ed2178c83ae
May <MayyFaucher@gmail.com>
2021-11-01 17:01:19 -0700

    gave up on middleware bug, and simply manually inserted it into routes, half of which are created

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -5,16 +5,25 @@ const withAuth = require('../utils/auth');
 router.get("/", async (req, res) => {
     try {
         const dbBlogData = await Blog.findAll({
-            include: [ User ]
+            attributes: ["id", "title", "body", "user_id"],
+            include: [
+                {
+                    model: User,
+                    as: "user_id",
+                    attributes: ["user_name"],
+                }
+            ]
         });
 
-        // const allBlogs = dbBlogData.map((blogs) =>
-        //     blogs.get({ plain: true })
-        // );
-        // res.render("homepage", {
-        //     allBlogs,
-        //     loggedIn: req.session.loggedIn,
-        // });
+        const allBlogs = dbBlogData.map((blogs) =>
+            blogs.get({ plain: true })
+        );
+
+        res.render("homepage", {
+            allBlogs,
+            loggedIn: req.session.loggedIn,
+        });
+
         // res.send("OKAY")
         res.status(200).json(dbBlogData);
     } catch (err) {
@@ -22,52 +31,50 @@ router.get("/", async (req, res) => {
     }
 });
 
-// router.get("/blog/:id", authorized, async (req, res) => {
-//     try {
-//         const dbBlogData = await Blog.findByPk(req.params.id, {
-//             include: [
-//                 {
-//                     model: Comment,
-//                     attributes: [
-//                         "id",
-//                         "content",
-//                         "blog_id",
-//                         "user_id"
-//                     ],
-//                 },
-//             ],
-//         });
-//         const dataSet = dbBlogData.get({ plain: true });
-//         res.render("blog", { dataSet, loggedIn: req.session.loggedIn });
-//     } catch (err) {
-//         res.status(500).json(err);
-//     }
-// });
-
-// router.get("/comment/:id", withAuth, async (req,res) => {
-//     try {
-//         const dbCommentData = await Comment.findByPK(req.params.id);
-//         const com = dbCommentData.get({ plain: true });
-//         res.render("comment", { com, loggedIn: req.session.loggedIn });
-//     } catch (err) {
-//         // console.log(err);
-//         res.status(500).json(err);
-//     }
-// })
-
-
-
-
-
-router.get('/profile', async (req, res) => {
-    try {
-        res.send("HEY");
-    } catch (err) {
-        res.status(500).json(err);
+router.get("/blog/:id", async (req, res) => {
+    if (!req.session.loggedIn) {
+        res.redirect('/login');
+    } else {
+        try {
+            const dbBlogData = await Blog.findByPk(req.params.id, {
+                include: [
+                    {
+                        model: Comment,
+                        attributes: [
+                            "id",
+                            "content",
+                            "blog_id",
+                            "user_id"
+                        ],
+                    },
+                ],
+            });
+            const dataSet = dbBlogData.get({ plain: true });
+            res.render("blog", { dataSet, loggedIn: req.session.loggedIn });
+        } catch (err) {
+            res.status(500).json(err);
+        }
     }
 });
 
+router.get("/comment/:id", async (req, res) => {
+    if (!req.session.loggedIn) {
+        res.redirect('/login');
+    } else {
+        try {
 
+            const dbCommentData = await Comment.findByPK(req.params.id);
+
+            const com = dbCommentData.get({ plain: true });
+
+            res.render("comment", { com, loggedIn: req.session.loggedIn });
+
+        } catch (err) {
+            // console.log(err);
+            res.status(500).json(err);
+        }
+    }
+    })
 
 
 
