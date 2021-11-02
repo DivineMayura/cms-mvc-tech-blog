ed25644abd3c2fc37b15d1953f8b88eb1f4f7774
May <MayyFaucher@gmail.com>
2021-11-01 22:37:18 -0700

    fully extablished all routes, all that's left is dashboard

---
+++ controllers/api/dashboard.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+const { Blog, User } = require('../../models');
+
+
+
+module.exports = router;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -29,7 +29,7 @@ router.get("/", async (req, res) => {
 });
 
 router.get("/blog/:id", async (req, res) => {
-    if (req.session.loggedIn) {
+    if (!req.session.loggedIn) {
         res.redirect('/login');
     } else {
         try {
