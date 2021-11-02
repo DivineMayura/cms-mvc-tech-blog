91ec975be74990cb6aa0ec20f0d1e54a7ae3249e
May <MayyFaucher@gmail.com>
2021-11-01 23:34:15 -0700

    added delete and push dashboard routes

--- controllers/api/dashboard.js
+++ controllers/api/dashboard.js
@@ -20,4 +20,42 @@ router.post("/", async (req, res) => {
       }
 });
 
+router.put('/:id', async (req, res) => {
+    // update a tag's name by its `id` value
+    try {
+      const blogUpdate = await Tag.update(
+        { description: req.body.description },
+        { where: {
+          id: req.params.id,
+        },
+      });
+  
+      if (!blogUpdate) {
+        res.status(404).json({ message: "No blog found with that id" });
+        return;
+      }
+      // res.status(200).json(blogUpdate);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+router.delete("/:id", async (req,res) => {
+    try {
+        const deleteABlog = await Blog.destroy({
+            where: { id: req.params.id },
+        });
+
+        if(!deleteABlog) {
+            res.status(404).json({ message: "No blog found with that id!" });
+            return;
+        }
+        res.status(200).json(deleteABlog);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+
 module.exports = router;
