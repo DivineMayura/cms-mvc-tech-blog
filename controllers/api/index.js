13ff1e9b1aff27ad5aee2fe94826ce4ac0a9d30f
May <MayyFaucher@gmail.com>
2021-11-01 21:11:26 -0700

    user POST route doesn't work.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 // const dashboard = require("./dashboard");
 const userRoutes = require('./userRoutes');
 
-router.use("/userRoutes", userRoutes);
+router.use("/userRoute", userRoutes);
 // router.use("/dashboard", dashboard);
 
 module.exports = router;
--- public/js/login.js
+++ public/js/login.js
@@ -1,20 +1,20 @@
 const loginFormHandler = async (event) => {
-    event.preventDefault();
-  
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
-  
-    if (email && password) {
-      const response = await fetch('/api/userRoutes/login', {
+  event.preventDefault();
+
+  const email = document.querySelector('#email-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
+
+  if (email && password) {
+      const response = await fetch('/api/userRoute/login', {
         method: 'POST',
         body: JSON.stringify({ email, password }),
         headers: { 'Content-Type': 'application/json' },
       });
   
       if (response.ok) {
-        document.location.replace('/');
+        document.location.replace('/profile');
       } else {
-        alert('Failed to log in.');
+        alert(response.statusText);
       }
     }
   };
@@ -22,21 +22,21 @@ const loginFormHandler = async (event) => {
   const signupFormHandler = async (event) => {
     event.preventDefault();
   
-    const username = document.querySelector('#username-signup').value.trim();
+    const name = document.querySelector('#name-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
   
-    if (username && email && password) {
-      const response = await fetch('/api/userRoutes', {
+    if (name && email && password) {
+      const response = await fetch('/api/userRoute', {
         method: 'POST',
-        body: JSON.stringify({ username, email, password }),
+        body: JSON.stringify({ name, email, password }),
         headers: { 'Content-Type': 'application/json' },
       });
   
       if (response.ok) {
-        document.location.replace('/');
+        document.location.replace('/profile');
       } else {
-        alert('Failed to sign up.');
+        alert(response.statusText);
       }
     }
   };
